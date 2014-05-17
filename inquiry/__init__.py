619a791ed65bce65d658d188ec56ab313df6b0ea
stevepeak <steve@stevepeak.net>
2014-05-17 14:27:40 -0400

    move validators

--- inquiry/__init__.py
+++ inquiry/__init__.py
@@ -1,3 +1,4 @@
 version = __version__ = VERSION = "0.0.1"
 
+from .validators import *
 from .inquiry import Inquiry
--- inquiry/garden.py
+++ inquiry/garden.py
@@ -6,33 +6,6 @@ from .helpers import *
 from .query import Query
 
 
-class where(valideer.Pattern):
-    name = "where"
-    regexp = re.compile(r"[\w&<>|*/+\-\(\)]+")
-    rp = re.compile(r"(\w+)")
-    def validate(self, value, adapt=True):
-        super(where, self).validate(value)
-        keys = self.rp.findall(value)
-        value = self.rp.sub(r'%(\1)s', value)
-        value = value.replace('|', ' or ')
-        value = value.replace('&', ' and ')
-        return keys, '('+value+')'
-
-
-class boolean(valideer.Validator):
-    name = "bool"
-    def validate(self, value, adapt=True):
-        if value in (False, True):
-            return value
-        elif value.lower() in ('f', 'false', '0'):
-            return False
-        elif value.lower() in ('t', 'true', '1'):
-            return True
-        else:
-            raise valideer.ValidationError("Bool is not a valid format")
-
-
-
 class Garden(object):
     """A garden is the container and controller for Figures
     the objective is to create a query from user arguments
